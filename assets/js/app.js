f6c8bde154e1198f59833b8ccff96f2a95c404d2
deepakjaat07 <deepakjhajhari4657@gmail.com>
2023-10-30 09:38:31 +0530

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,12 +10,12 @@ function opentab(Tabid) {
     opentabs.classList.remove("d-none");
 }
 function closetab(Tabid) {
-    const closetabs = document.querySelectorAll(".closeBtn");
-    closetabs.forEach((closeTab) => {
-        closeTab.classList.add("d-none");
-        closeTab.classList.remove("d-block");
+    const tabs = document.querySelectorAll(".tabs");
+    tabs.forEach((openTab) => {
+        openTab.classList.add("d-block");
+        openTab.classList.remove("d-none");
     });
     const opentabs = document.getElementById(Tabid);
-    opentabs.classList.add("d-block");
-    opentabs.classList.remove("d-none");
+    opentabs.classList.add("d-none");
+    opentabs.classList.remove("d-block");
 }
